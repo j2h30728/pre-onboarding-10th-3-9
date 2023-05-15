e16f97c72ec1dd9068840d58016ef24398fe5adc
j2h30728 <60846068+j2h30728@users.noreply.github.com>
2023-05-15 23:12:13 +0900

    feat: 프로젝트 세팅 (#2)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,22 @@
-export {};
+import axios, { AxiosRequestConfig } from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL;
+const token = process.env.REACT_APP_TOKEN;
+
+const baseInstance = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+baseInstance.interceptors.response.use(({ data }) => data);
+
+const apiRequest = {
+  get: <T>(url: string, request?: AxiosRequestConfig) => baseInstance.get<T>(url, request),
+  delete: <T>(url: string, request?: AxiosRequestConfig) => baseInstance.delete<T>(url, request),
+  post: <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
+    baseInstance.post<T>(url, data, config),
+};
+
+export default apiRequest;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+import { Todo } from '../@types/todos';
+import TodoItem from './TodoItem';
+
+interface TodoListProps {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList = ({ todos, setTodos }: TodoListProps) => {
+  return todos.length ? (
+    <ul>
+      {todos.map(({ id, title }) => (
+        <TodoItem key={id} id={id} title={title} setTodos={setTodos} />
+      ))}
+    </ul>
+  ) : (
+    <div className="empty-list">...</div>
+  );
+};
+export default TodoList;
